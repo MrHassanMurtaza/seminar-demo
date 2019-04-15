a421b20f5121223ca1c96f2f2be7f13da698f8f1
zaheersani <sani.zaheer@gmail.com>
2019-04-15 15:43:00 +0500

    Backend Code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def add(num1, num2):
 def sub(num1, num2):
   return num1-num2
 
+def Backend(server):
+  # Server backend code goes here!
+
 if __name__ == "__main__":
     print(add(4,6))
     print(sub(10,6))
