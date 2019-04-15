d4e2127553f6d87940cc5c306a6fafe2c591b90c
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-04-15 15:30:57 +0500

    Changing the variable names

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def add(num1, num2):
   return num1 + num2
 
 # Added by Zaheer
-def sub(num1, num2):
-  return num1 - num2
+def sub(a, b):
+  return a - b
 
 if __name__ == "__main__":
     print(add(4,6))
